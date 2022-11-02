2588754d22851fa87faa549adede5eac6f9331cf
Dominike Righi <111894682+domisrighi@users.noreply.github.com>
2022-11-02 20:23:54 -0300

    Update ArvoreBinaria.cpp

--- ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
@@ -79,7 +79,7 @@ void menu()
 void inicializar()
 {
 
-	// provisÛrio porque n„o libera a memoria usada pela arvore
+	// provis√≥rio porque n√£o libera a memoria usada pela arvore
 	NO* raiz = NULL;
 
 	cout << "Arvore inicializada \n";
@@ -177,10 +177,15 @@ void exibirElementosArvore(NO* no)
 
 void buscarElementoArvore(NO* no, int valor)
 {
+	if (no == NULL)
+		cout << "Elemento nao encontrado!" << endl;
+
+	else if (no->valor == valor) 
+		cout << "Elemento encontrado: " << valor << endl;
+
+	else if (no->valor > valor)
+		buscarElementoArvore(no->esq, valor);
 	
-
+	else
+		buscarElementoArvore(no->dir, valor);	
 }
-
-
-
-
